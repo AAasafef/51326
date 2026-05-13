8edfaea62fd46ae432ce1f9835d6544cedf55650
AAasafef <153408365+AAasafef@users.noreply.github.com>
2026-05-13 18:20:10 -0400

    Update app_config.dart

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,18 +1,9 @@
 class AppConfig {
 
-  // DEVELOPMENT
-  // -------------------
-  // TRUE:
-  // Skip onboarding/setup
-  // and open dashboard directly.
-  //
-  // FALSE:
-  // Show onboarding flow.
   static const bool
       skipOnboardingForDevelopment =
       true;
 
-  // FUTURE
   static const bool
       developerModeEnabled =
       true;
